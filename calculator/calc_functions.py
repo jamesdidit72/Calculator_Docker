616dd334e41913d483043380d8194b7cce47b183
jamesdidit72 <jamesjbarton72@gmail.com>
2021-06-28 10:11:14 +0100

    web calculator

---
+++ calculator/calc_functions.py
@@ -0,0 +1,17 @@
+def add(num1, num2):
+    num3 = num1 + num2
+    return num3
+def subtract(num1, num2):
+    num3 = num1 - num2
+    return num3
+
+def multiply(num1, num2):
+    num3 = num1 * num2
+    return num3
+
+def divide(num1, num2):
+    if num1 == 0 or num2 == 0:
+        return none
+    else:
+        num3 = num1 / num2
+        return num3
---
+++ calculator/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, abort
+import calc_functions as calc_functions
+
+def read_num(num_id):
+    num = input(f"enter integer {num_id}: ")
+    return int(num)
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the Calulator app'
+
+@app.route('/add/<int:num1>/<int:num2>')
+def perform_add(num1, num2):
+    num3 = calc_functions.add(num1, num2)
+    print(num3)
+    return str(num3)
+
+# using query string
+# URL: 127.0.0.1:5000/add?num1=1&num2=2
+@app.route('/add')
+def perform_add_query():
+    num1 = request.args.get('num1', type =int, default = 0)
+    num2 = request.args.get('num2', type = int, default = 0)
+
+    if num1 is None or num2 is None:
+        abort(404)
+    num3 = calc_functions.add(num1, num2)
+    print(num3)
+    return str(num3)
+
+if __name__ == '__main__':
+    app.run(debug= True, host = '0.0.0.0')
+# if __name__ == '__main__':
+    # num1 = read_num(1)
+    # num2 = read_num(2)
+
+    # result = calc_functions.add(num1, num2)
+    # print(result)
